81098f92885f21194d868d7336aa4000e3fdad9f
Sammy459 <gupta.samridh.484@gmail.com>
2023-11-13 17:02:44 +0530

    New class

---
+++ src/main/java/com/example/approject/Background.java
@@ -0,0 +1,12 @@
+public class Background  {
+    private int color=0;
+    Background(int color){
+        this.color=color;
+    }
+    public int getColor(){
+        return color;
+    }
+    public void setColor(int color){
+        this.color=color;
+    }
+}
