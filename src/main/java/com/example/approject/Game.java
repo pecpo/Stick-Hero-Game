2f6b6b412ac2b1bba3d999821c3ca0b17ac16c53
Sammy459 <gupta.samridh.484@gmail.com>
2023-11-14 21:50:29 +0530

    initial commit

--- src/main/java/com/example/approject/Game.java
+++ src/main/java/com/example/approject/Game.java
@@ -3,11 +3,24 @@ package com.example.approject;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import static javafx.application.Application.launch;
 
-public class Game {
+public class Game extends Application {
+    @Override
     public void start(Stage stage) throws IOException {
         try{
+            stage.setTitle("Home Screen");
+            Button start=new Button("Start");
+            Button exit=new Button("Exit");
+            HBox hbox = new HBox(10);
+            hbox.getChildren().addAll(start, exit);
+            Scene scene=new Scene(hbox,1000,500);
+            stage.setScene(scene);
+            stage.show();
             Player player = new Player();
             GamePlatform gamePlatform = new GamePlatform(100, 0, 100, 0, 0);
             Scoreboard scoreboard = new Scoreboard();
@@ -16,7 +29,6 @@ public class Game {
         }
         catch (Exception e){
             e.printStackTrace();
-
         }
 
     }
