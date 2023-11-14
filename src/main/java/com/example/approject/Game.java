898b95e0e9434c890c463f0bc18e6bb21f171963
Sammy459 <gupta.samridh.484@gmail.com>
2023-11-14 22:36:39 +0530

    initial commit

--- src/main/java/com/example/approject/Game.java
+++ src/main/java/com/example/approject/Game.java
@@ -1,12 +1,17 @@
 package com.example.approject;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import static javafx.application.Application.launch;
 
 public class Game extends Application {
@@ -14,11 +19,15 @@ public class Game extends Application {
     public void start(Stage stage) throws IOException {
         try{
             stage.setTitle("Home Screen");
-            Button start=new Button("Start");
-            Button exit=new Button("Exit");
-            HBox hbox = new HBox(10);
-            hbox.getChildren().addAll(start, exit);
-            Scene scene=new Scene(hbox,1000,500);
+            Button welcome=new Button("Welcome");
+            Image image=new Image("stick.png");
+            ImageView imageview=new ImageView(image);
+            Pane layout = new Pane();
+            layout.getChildren().add(imageview);
+            layout.getChildren().add(welcome);
+            welcome.setLayoutX(240);
+            welcome.setLayoutY(600);
+            Scene scene=new Scene(layout,500,800);
             stage.setScene(scene);
             stage.show();
             Player player = new Player();
