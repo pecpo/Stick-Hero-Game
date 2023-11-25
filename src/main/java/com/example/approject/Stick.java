e67307c50cc70ec5f986d0c0eee5372c5b26170c
Sammy459 <gupta.samridh.484@gmail.com>
2023-11-25 11:57:24 +0530

    Changes

--- src/main/java/com/example/approject/Stick.java
+++ src/main/java/com/example/approject/Stick.java
@@ -1,26 +1,48 @@
 package com.example.approject;
 
-public class Stick {
-    private double length;
+import javafx.animation.Timeline;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
-    // Constructor
+public class Stick {
+    private static double length;
+    private static double initial;
+    private final Line StickLine=new Line();
     public Stick() {
-        this.length = 0;
+        length = 0;
+        initial =0;
+        StickLine.setStroke(Color.BLACK);
     }
 
     public double getLength() {
         return length;
     }
 
-    public void setLength(double length) {
-        this.length = length;
+    public void setLength(double lengths) {
+        length = lengths;
     }
 
-    public void extend(){
-
+    public void extend(Scene scene, Timeline extendTimeline){
+        scene.setOnMousePressed(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                initial =event.getX();
+                StickLine.setStartX(initial);
+                StickLine.setStartY(0);
+                StickLine.setEndX(initial);
+                StickLine.setEndY(0);
+                extendTimeline.play();
+            }
+        });
     }
 
-    public void reset() {
-        length = 0;
+    public void reset(Scene scene, Timeline extendTimeline) {
+        scene.setOnMouseReleased(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                extendTimeline.stop();
+            }
+        });
+        length=StickLine.getEndX()-StickLine.getStartX();
     }
 }
