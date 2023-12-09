d822d8698d9b012b74d85ac98beb71dea6797f0e
Sammy459 <gupta.samridh.484@gmail.com>
2023-12-09 22:37:57 +0530

    updating

--- src/main/java/com/example/approject/GameTest.java
+++ src/main/java/com/example/approject/GameTest.java
@@ -4,24 +4,26 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameTest {
-//    @BeforeClass
-//    public Game createGame(){
-//        Game game=new Game();
-//        return game;
-//    }
-//    @Test
-//    public void Test1() { //checks if character is flipped initially
-//
-//        Assert.assertFalse(game.isIsFlipped());
-//    }
-//    @Test
-//    public void Test2(){
-//        Player player=Player.getPlayer();
-//        Assert.assertNull(player);
-//    }
-//    @Test
-//    public void Test3(){
-//        Game game=new Game();
-//        Assert.assert
-//    }
+    public static Game game=null;
+    @BeforeClass
+    public static void createGame(){
+        game=new Game();
+    }
+    @Test
+    public void Test1() { //checks if character is flipped initially
+        Assert.assertFalse(game.isIsFlipped());
+    }
+    @Test
+    public void Test2(){ //checks our singleton design pattern
+        Player player=Player.getPlayer();
+        Assert.assertNotNull(player);
+    }
+    @Test
+    public void Test3(){ //check if initial score of character is zero
+        Assert.assertEquals(0,game.getCurrentScore());
+    }
+    @Test
+    public void Test4(){ //check if initial score of character is zero
+        Assert.assertEquals(0,game.getCurrentScore());
+    }
 }
