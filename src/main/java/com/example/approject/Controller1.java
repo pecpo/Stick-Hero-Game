36f40b90e780e82afedf75d076a9359376713caf
Sammy459 <gupta.samridh.484@gmail.com>
2023-12-09 21:31:00 +0530

    updating

--- src/main/java/com/example/approject/Controller1.java
+++ src/main/java/com/example/approject/Controller1.java
@@ -12,15 +12,36 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
 public class Controller1 {
+    public static String getCharacter() {
+        return Character;
+    }
+
+    private static String Character="character.png";
     @FXML
     private ImageView myCharacter;
     @FXML
     private void switchCharacter1(ActionEvent event) throws IOException {
         Image image=new Image("sword_hero.png");
         myCharacter.setImage(image);
+//        Path sourcePath = Paths.get("src/main/resources/sword_hero.png");
+//        Path destinationPath = Paths.get("src/main/resources/character.png");
+//        try {
+//            // Copy the image using Files.copy method
+//            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+//
+//            System.out.println("Image copied successfully!");
+//        } catch (IOException e) {
+//            // Handle IOException, e.g., file not found, permission issues, etc.
+//            e.printStackTrace();
+//        }
+        Character="sword_hero.png";
     }
     @FXML
     private void switchCharacter2(ActionEvent event) throws IOException {
--- src/main/java/com/example/approject/Main.java
+++ src/main/java/com/example/approject/Main.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 import javafx.scene.Scene;
 import  java.io.*;
@@ -14,6 +18,17 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
+//            Path sourcePath = Paths.get("src/main/resources/original.png");
+//            Path destinationPath = Paths.get("src/main/resources/character.png");
+//            try {
+//                // Copy the image using Files.copy method
+//                Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+//
+//                System.out.println("Image copied successfully!");
+//            } catch (IOException e) {
+//                // Handle IOException, e.g., file not found, permission issues, etc.
+//                e.printStackTrace();
+//            }
             FXMLLoader loader = new FXMLLoader(getClass().getResource("scene1.fxml"));
             Parent root= loader.load();
             Controller1 controller1 = loader.getController();
